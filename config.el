609909c8578133c3ede7e207f0ad50db606a2930
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2023-03-21 20:59:15 +0100

    Initial commit

---
+++ config.el
@@ -0,0 +1,166 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+(setq user-full-name "Daniel Tingelöf"
+      user-mail-address "Daniel.tingelof@ericsson.com")
+(setq doom-theme 'doom-one)
+(setq display-line-numbers-type 'relative)
+(setq org-directory "~/org/")
+(setq-default
+ delete-by-moving-to-trash t                      ; Delete files to trash
+ window-combination-resize t                      ; take new window space from all other windows (not just current)
+ x-stretch-cursor t)                              ; Stretch cursor to the glyph width
+
+(setq undo-limit 80000000                         ; Raise undo-limit to 80Mb
+      evil-want-fine-undo t                       ; By default while in insert all changes are one big blob. Be more granular
+      auto-save-default t                         ; Nobody likes to loose work, I certainly don't
+      truncate-string-ellipsis "…"                ; Unicode ellispis are nicer than "...", and also save /precious/ space
+      password-cache-expiry nil                   ; I can trust my computers ... can't I?
+      ;; scroll-preserve-screen-position 'always     ; Don't have `point' jump around
+      scroll-margin 2                             ; It's nice to maintain a little margin
+      display-time-default-load-average nil)      ; I don't think I've ever found this useful
+
+(display-time-mode 1)                             ; Enable time in the mode-line
+
+(unless (string-match-p "^Power N/A" (battery))   ; On laptops...
+  (display-battery-mode 1))                       ; it's nice to know how much power you have
+
+(global-subword-mode 1)                           ; Iterate through CamelCase words
+
+(add-to-list 'default-frame-alist '(height . 24))
+(add-to-list 'default-frame-alist '(width . 80))
+
+(setq evil-vsplit-window-right t
+      evil-split-window-below t)
+
+(defadvice! prompt-for-buffer (&rest _)
+  :after '(evil-window-split evil-window-vsplit)
+  (consult-buffer))
+
+(map! :map evil-window-map
+      "SPC" #'rotate-layout
+      ;; Navigation
+      "<left>"     #'evil-window-left
+      "<down>"     #'evil-window-down
+      "<up>"       #'evil-window-up
+      "<right>"    #'evil-window-right
+      ;; Swapping windows
+       "C-<left>"       #'+evil/window-move-left
+       "C-<down>"       #'+evil/window-move-down
+       "C-<up>"         #'+evil/window-move-up
+       "C-<right>"      #'+evil/window-move-right)
+(map!
+      "C-<left>"     #'evil-window-left
+      "C-<down>"     #'evil-window-down
+      "C-<up>"       #'evil-window-up
+      "C-<right>"    #'evil-window-right)
+
+(map! :leader
+      (:desc "Quit Buffer" "q" #'evil-quit)
+      (:desc "Quit All Buffers" "Q" #'evil-quit-all))
+
+(evil-define-command +evil-buffer-org-new (count file)
+  "Creates a new ORG buffer replacing the current window, optionally
+   editing a certain FILE"
+  :repeat nil
+  (interactive "P<f>")
+  (if file
+      (evil-edit file)
+    (let ((buffer (generate-new-buffer "*new org*")))
+      (set-window-buffer nil buffer)
+      (with-current-buffer buffer
+        (org-mode)
+        (setq-local doom-real-buffer-p t)))))
+
+(map! :leader
+      (:prefix "b"
+       :desc "New empty Org buffer" "o" #'+evil-buffer-org-new))
+
+;; (map! "C-s" #'counsel-grep-or-swiper)
+(map! "C-s" #'+default/search-buffer)
+(after! smartparens
+  (defun zz/goto-match-paren (arg)
+    "Go to the matching paren/bracket, otherwise (or if ARG is not
+    nil) insert %.  vi style of % jumping to matching brace."
+    (interactive "p")
+    (if (not (memq last-command '(set-mark
+                                  cua-set-mark
+                                  zz/goto-match-paren
+                                  down-list
+                                  up-list
+                                  end-of-defun
+                                  beginning-of-defun
+                                  backward-sexp
+                                  forward-sexp
+                                  backward-up-list
+                                  forward-paragraph
+                                  backward-paragraph
+                                  end-of-buffer
+                                  beginning-of-buffer
+                                  backward-word
+                                  forward-word
+                                  mwheel-scroll
+                                  backward-word
+                                  forward-word
+                                  mouse-start-secondary
+                                  mouse-yank-secondary
+                                  mouse-secondary-save-then-kill
+                                  move-end-of-line
+                                  move-beginning-of-line
+                                  backward-char
+                                  forward-char
+                                  scroll-up
+                                  scroll-down
+                                  scroll-left
+                                  scroll-right
+                                  mouse-set-point
+                                  next-buffer
+                                  previous-buffer
+                                  previous-line
+                                  next-line
+                                  back-to-indentation
+                                  doom/backward-to-bol-or-indent
+                                  doom/forward-to-last-non-comment-or-eol
+                                  )))
+        (self-insert-command (or arg 1))
+      (cond ((looking-at "\\s\(") (sp-forward-sexp) (backward-char 1))
+            ((looking-at "\\s\)") (forward-char 1) (sp-backward-sexp))
+            (t (self-insert-command (or arg 1))))))
+  (map! "%" 'zz/goto-match-paren))
+
+
+;; Org mode
+(after! org (setq org-hide-emphasis-markers t))
+(after! org (setq org-insert-heading-respect-content nil))
+
+(defun zz/add-file-keybinding (prefix key file &optional desc)
+  (let ((prefix prefix)
+        (key key)
+        (file file)
+        (desc desc))
+    (map! :leader
+          (:prefix prefix
+           :desc (or desc file)
+           key
+           (lambda () (interactive) (find-file file))))))
+(zz/add-file-keybinding "n" "d" "~/org/daniel.org" "Daniels Notes")
+
+;; other
+(map! :n [mouse-8] #'better-jumper-jump-backward
+      :n [mouse-9] #'better-jumper-jump-forward)
+
+(setq projectile-ignored-projects
+      (list "~/" "/tmp" (expand-file-name "straight/repos" doom-local-dir)))
+
+(setq projectile-project-root-files
+      (list "bob" "pom.xml" ".git" "ruleset2.0.yaml"))
+(setq projectile-project-search-path '("~/.config/doom" ("~/repos_personal" . 1)("~/repos" . 1)))
+(defun projectile-ignored-project-function (filepath)
+  "Return t if FILEPATH is within any of `projectile-ignored-projects'"
+  (or (mapcar (lambda (p) (s-starts-with-p p filepath)) projectile-ignored-projects)))
+
+(eval-after-load 'evil-vars
+ '(define-key evil-insert-state-map (kbd "C-v") 'yank))
+
+(eval-after-load 'evil-vars
+ '(define-key evil-ex-completion-map (kbd "C-v") 'yank))
---
+++ init.el
@@ -0,0 +1,87 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; This file controls what Doom modules are enabled and what order they load
+;; in. Remember to run 'doom sync' after modifying it!
+
+;; NOTE Press 'SPC h d h' (or 'C-h d h' for non-vim users) to access Doom's
+;;      documentation. There you'll find a link to Doom's Module Index where all
+;;      of our modules are listed, including what flags they support.
+
+;; NOTE Move your cursor over a module's name (or its flags) and press 'K' (or
+;;      'C-c c k' for non-vim users) to view its documentation. This works on
+;;      flags as well (those symbols that start with a plus).
+;;
+;;      Alternatively, press 'gd' (or 'C-c c d') on a module to browse its
+;;      directory (for easy access to its source code).
+
+(doom! :input
+       :completion
+       (company +childframe)           ; the ultimate code completion backend
+       (vertico +icons)          ; the search engine of the future
+
+       :ui
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       indent-guides     ; highlighted indent columns
+       ligatures         ; ligatures and symbols to make your code pretty again
+       minimap           ; show a map of the code on the side
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       nav-flash         ; blink cursor line after big motions
+       neotree           ; a project drawer, like NERDTree for vim
+       ophints           ; highlight the region an operation acts on
+       (popup +defaults)   ; tame sudden yet inevitable temporary windows
+       tabs              ; a tab bar for Emacs
+       ;;treemacs          ; a project drawer, like neotree but cooler
+       (vc-gutter +pretty) ; vcs diff in the fringe
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       workspaces        ; tab emulation, persistence & separate workspaces
+
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
+       snippets          ; my elves. They type so I don't have to
+       word-wrap         ; soft wrapping with language-aware indent
+       format
+
+       :emacs
+       dired             ; making dired pretty [functional]
+       electric          ; smarter, keyword-based electric-indent
+       undo              ; persistent, smarter undo for your inevitable mistakes
+       vc                ; version-control and Emacs, sitting in a tree
+
+       :term
+       vterm             ; the best terminal emulation in Emacs
+
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+
+       :tools
+       (docker +lsp)
+       (eval +overlay)     ; run code, run (also, repls)
+       lookup              ; navigate your code and its documentation
+       (lsp +eglot +peek)               ; M-x vscode
+       magit             ; a git porcelain for Emacs
+       ;;tmux              ; an API for interacting with tmux
+       tree-sitter       ; syntax and parsing, sitting in a tree...
+
+       :os
+       (:if IS-MAC macos)  ; improve compatibility with macOS
+       ;;tty               ; improve the terminal Emacs experience
+
+       :lang
+       emacs-lisp        ; drown in parentheses
+       (go +lsp)         ; the hipster dialect
+       (json + lsp)              ; At least it ain't XML
+       (java +lsp)       ; the poster child for carpal tunnel syndrome
+       (lua +lsp)               ; one-based indices? one-based indices
+       markdown          ; writing docs for people to ignore
+       org               ; organize your plain life in plain text
+       (python +lsp +pyright)            ; beautiful is better than ugly
+       (sh +lsp)               ; she sells {ba,z,fi}sh shells on the C xor
+       (yaml +lsp)              ; JSON, but readable
+
+       :config
+       ;;literate
+       (default +bindings +smartparens))
